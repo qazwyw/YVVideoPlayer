0cf02046d3c9686cb437daa57ca51c5203dac4d2
Yvan <wuyuewen@waijiaojun.com>
2017-01-21 17:52:57 +0800

    readme update

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # YVVideoPlayer
 一个基于AVPlayer的视频播放demo，用法：
-    1.创建一个容器视图containerView
-    2.用-initWithContainerView:初始化YvVideoPlayer
+    1.用类方法+playerWithFrame实例化YvVideoPlayer
+    2.添加player到视图中
     3.调用实例方法-playWithVideoUrlString:播放网络视频
--- VideoPlay/ViewController.m
+++ VideoPlay/ViewController.m
@@ -20,8 +20,6 @@
 #pragma mark - 控制器视图方法
 - (void)viewDidLoad {
     [super viewDidLoad];
-//    UIView *containerView = [[UIView alloc]initWithFrame:CGRectMake(10, 20, CGRectGetWidth([UIScreen mainScreen].bounds) - 20, 300)];
-   
     self.player = [YvVideoPlayer playerWithFrame:CGRectMake(10, 20, CGRectGetWidth([UIScreen mainScreen].bounds) - 20, 300)];
     [self.view addSubview:self.player];
     [self.player playWithVideoUrlString:@"http://v.jxvdy.com/sendfile/w5bgP3A8JgiQQo5l0hvoNGE2H16WbN09X-ONHPq3P3C1BISgf7C-qVs6_c8oaw3zKScO78I--b0BGFBRxlpw13sf2e54QA"];
